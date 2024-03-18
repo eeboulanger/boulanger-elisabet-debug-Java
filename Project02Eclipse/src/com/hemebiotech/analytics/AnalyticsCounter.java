c9ca2abbb78a0bd4fc940f945115b6f00c24bf54
Liza <eboulanger.pro@gmail.com>
2024-03-18 14:26:16 +0100

    Removed unnecessary comments Fixed the code, so it generates correct value per symptom

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,33 +5,41 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount;
+	private static int rashCount;
+	private static int pupilCount;
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader
+				("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int tempHeadCount = 0;
+		int tempRashCount=0;
+		int tempPupilCount=0;
+
 		while (line != null) {
-			i++;	// increment i
+
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				tempHeadCount++;
+				System.out.println("number of headaches: " + tempHeadCount);
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
+			else if (line.equals("rash")) {
+				tempRashCount++;
 			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+			else if (line.contains("dialated pupils")) {
+				tempPupilCount++;
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
+
+
+		headacheCount =tempHeadCount;
+		rashCount =tempRashCount;
+		pupilCount =tempPupilCount;
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
